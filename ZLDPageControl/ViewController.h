52bff6fe4b3793c541928073af7fa65e22054375
ZhuLida <lida.zhu@meelive.cn>
2016-08-04 17:09:32 +0800

    Initial Commit

---
+++ ZLDPageControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZLDPageControl
+//
+//  Created by zld on 8/4/16.
+//  Copyright © 2016 zld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZLDPageControl/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZLDPageControl
+//
+//  Created by zld on 8/4/16.
+//  Copyright © 2016 zld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
